567276ccc1e937ecc7c498f8bda47dda00de0aed
kauandoti <98996298+kauandoti@users.noreply.github.com>
2023-09-12 22:32:43 -0300

    tarefa 19

--- trabalho1.sql
+++ trabalho1.sql
@@ -101,3 +101,9 @@ SELECT alunos.nome AS aluno, COUNT(matriculas.id) AS total_de_matriculas
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
 GROUP BY alunos.nome;
+
+/*19ª partte*/
+SELECT alunos.nome AS aluno, COUNT(matriculas.id) AS total_de_matriculas
+FROM alunos
+LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
+GROUP BY alunos.nome;
