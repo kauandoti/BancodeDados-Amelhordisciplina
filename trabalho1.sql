b117bbf6286d462c182ccea01736d1d94b852eed
kauandoti <98996298+kauandoti@users.noreply.github.com>
2023-09-12 22:41:27 -0300

    outras mudanças

--- trabalho1.sql
+++ trabalho1.sql
@@ -90,11 +90,6 @@ GROUP BY produto
 ORDER BY receita_total ASC
 LIMIT 1;
 
---19ª partte
-SELECT alunos.nome AS aluno, COUNT(matriculas.id) AS total_de_matriculas
-FROM alunos
-LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
-GROUP BY alunos.nome;
 
 --19ª partte
 SELECT alunos.nome AS aluno, COUNT(matriculas.id) AS total_de_matriculas
