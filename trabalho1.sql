2487459b97e20e4bf70dee34118f0e09dd00a582
kauandoti <98996298+kauandoti@users.noreply.github.com>
2023-09-12 22:34:13 -0300

    tarefa 20 teminada

--- trabalho1.sql
+++ trabalho1.sql
@@ -55,27 +55,27 @@ SELECT l.titulo AS livro, a.nome AS autor
 FROM livros AS l
 JOIN autores AS a ON l.autor_id = a.id;
 
-/*12ª partte*/
+--12ª partte
 SELECT alunos.nome AS aluno, matriculas.curso
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id;
 
-/*13ª partte*/
+--13ª partte
 SELECT a.nome AS autor, COALESCE(l.titulo, 'Sem Livros aqui') AS livro
 FROM autores AS a
 LEFT JOIN livros AS l ON a.id = l.autor_id;
 
-/*14ª partte*/
+--14ª partte
 SELECT alunos.nome AS aluno, COALESCE(matriculas.curso, 'Nenhum aluno registrado') AS curso
 FROM matriculas
 RIGHT JOIN alunos ON matriculas.aluno_id = alunos.id;
 
-/*15ª partte*/
+--15ª partte
 SELECT alunos.nome AS aluno, matriculas.curso
 FROM alunos
 INNER JOIN matriculas ON alunos.id = matriculas.aluno_id;
 
-/*16ª partte*/
+--16ª partte
 SELECT a.nome AS autor, COUNT(l.id) AS total_de_livros
 FROM autores AS a
 LEFT JOIN livros AS l ON a.id = l.autor_id
@@ -83,27 +83,34 @@ GROUP BY a.nome
 ORDER BY total_de_livros DESC
 LIMIT 1;
 
-/*17ª partte*/
+--17ª partte
 SELECT produto, SUM(receita) AS receita_total
 FROM vendas
 GROUP BY produto
 ORDER BY receita_total ASC
 LIMIT 1;
 
-/*18ª partte*/
+--18ª partte
 SELECT a.nome AS autor, SUM(20 * COUNT(l.id)) AS receita_total
 FROM autores AS a
 LEFT JOIN livros AS l ON a.id = l.autor_id
 GROUP BY a.nome;
 
-/*19ª partte*/
+--19ª partte
 SELECT alunos.nome AS aluno, COUNT(matriculas.id) AS total_de_matriculas
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
 GROUP BY alunos.nome;
 
-/*19ª partte*/
+--19ª partte
 SELECT alunos.nome AS aluno, COUNT(matriculas.id) AS total_de_matriculas
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
 GROUP BY alunos.nome;
+
+--20ª partte
+SELECT produto, COUNT(id) AS quantidade_de_transacoes
+FROM vendas
+GROUP BY produto
+ORDER BY quantidade_de_transacoes DESC
+LIMIT 1;
