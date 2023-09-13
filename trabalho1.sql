66eb6c145db49f0c41a07bca4d163b26645b4a9e
kauandoti <98996298+kauandoti@users.noreply.github.com>
2023-09-12 22:25:26 -0300

    tarefa 10

--- trabalho1.sql
+++ trabalho1.sql
@@ -1,44 +1,51 @@
 --1ª partte
 SELECT titulo FROM livros;
 
-/*2ª partte*/
+--2ª partte
 SELECT nome FROM autores WHERE nascimento < '1900-01-01';
 
-/*3ª partte*/
+--3ª partte
 SELECT l.titulo 
 FROM livros AS l 
 JOIN autores AS a ON l.autor_id = a.id 
 WHERE a.nome = 'J.K. Rowling';
 
-/*4ª partte*/
+--4ª partte
 SELECT alunos.nome 
 FROM alunos 
 JOIN matriculas ON alunos.id = matriculas.aluno_id 
 WHERE matriculas.curso = 'Engenharia de Software';
 
-/*5ª partte*/
+--5ª partte
 SELECT produto, SUM(receita) AS receita_total 
 FROM vendas 
 GROUP BY produto;
 
-/*6ª partte*/
+--6ª partte
 SELECT a.nome AS autor, COUNT(l.id) AS total_de_livros
 FROM autores AS a
 LEFT JOIN livros AS l ON a.id = l.autor_id
 GROUP BY a.nome;
 
-/*7ª partte*/
+--7ª partte
 SELECT curso, COUNT(aluno_id) AS total_de_alunos
 FROM matriculas
 GROUP BY curso;
 
-/*8ª partte*/
+--8ª partte
 SELECT produto, AVG(receita) AS media_de_receita
 FROM vendas
 GROUP BY produto;
 
-/*9ª partte*/
+--9ª partte
 SELECT produto, SUM(receita) AS receita_total
 FROM vendas
 GROUP BY produto
 HAVING receita_total > 10000;
+
+--10ª partte
+SELECT a.nome AS autor
+FROM autores AS a
+JOIN livros AS l ON a.id = l.autor_id
+GROUP BY a.nome
+HAVING COUNT(l.id) > 2;
